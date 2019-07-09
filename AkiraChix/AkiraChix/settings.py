1dc0151ea7faf842349d8551e7c40e4ac3df8811
gayagenius <gayaflo95@gmail.com>
2019-07-09 19:46:26 +0300

    adding django files

--- AkiraChix/AkiraChix/settings.py
+++ AkiraChix/AkiraChix/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'student',
+    'teacher',
+    'course',
+    'api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'student/media')
+MEDIA_ROOT = os.path.join(BASE_DIR,'teacher/media')
+
+django_heroku.settings(locals())
+
